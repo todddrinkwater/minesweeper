943a4180b704df86100d003f67449dab058b6634
todddrinkwater <todddrinkwaternz@gmail.com>
2017-01-29 10:33:13 +1300

    attempt to populate grid not working

--- minesweeper.js
+++ minesweeper.js
@@ -1,64 +1,96 @@
-document.addEventListener('DOMContentLoaded', startGame)
+document.addEventListener('DOMContentLoaded', startGame);
 
-// Define your `board` object here!
-var board = {
-  cells: [
-    {
-    row: 1,
-    col: 1,
-    isMine: false,
-    hidden: true
-  }, {
-    row: 1,
-    col: 2,
-    isMine: true,
-    hidden: true
-  }, {
-    row: 1,
-    col: 3,
-    isMine:true,
-    hidden: true
-  }, {
-    row: 2,
-    col: 1,
-    isMine:true,
-    hidden: true
-  },
-  {
-   row: 2,
-   col: 2,
-   isMine:false,
-   hidden: true
- },
- {
-  row: 2,
-  col: 3,
-  isMine:true,
-  hidden: true
-},
-{
- row: 3,
- col: 1,
- isMine:true,
- hidden: true
-}, {
-  row: 3,
-  col: 2,
-  isMine:false,
-  hidden: true
-}, {
-  row: 3,
-  col: 3,
-  isMine:true,
-  hidden: true
-},
+var board = { cells: [ ] };
 
-],
+// Check user's grid size is OK
+function checkInput(){
 
+  //Collect grid-size data from text input
+  var gridSize = document.getElementById("grid-input").value;
+
+  //If element is a number then proceed.
+  if (gridSize !== NaN){
+  //If input is greater than 0, and less than 100, create grid.
+      if (gridSize > 4 && gridSize < 30){
+        alert("Thank you!");
+        document.getElementById("board").innerHTML = createGrid();
+      }
+      else {
+        alert("Grid-size must be between 4 and 30!")
+      };
+  }
+  else {
+    alert("Sorry, your input must be a number.")
+  }
 };
 
 
+// Create grid with size based on gridSize input
+var createGrid(gridSize){
+
+      var countCol = 0;
+      var countRow = 0;
+
+      for(var i = 0; countRow < gridSize; i++) {
+        for(var i = 0; countCol < gridSize; i++){
+          board.cells.push(
+            {
+              row: countRow, col: countCol, isMine: Math.random() >= 0.2, isMarked: false, hidden: true
+            }
+          );
+          countCol = countCol + 1;
+        }
+        countCol = 0;
+        countRow = countRow + 1;
+      }
+    };
+
+
+  /*  var rowNumber = 1;
+    var rowCount = 1;
+    var colNumber = 1;
+    var colCount = 1;
+
+    for (var a = 0; a < gridSize; a++){
+      //Create Board object
+        board.cells[a] = new Object();
+
+      // Set all cells to HIDDDEN
+        board.cells[a].hidden = true;
+
+    //Create rows
+    if (rowCount <= gridSize){
+      board.cells[a].row = rowNumber;
+      rowCount++;
+    }
+
+    //If number of rows is greater than gridSize then reset rows to 1
+    if (rowCount >= gridSize){
+      rowNumber++;
+      rowCount = 1;
+    }
+
+    //Create columns and add 1 to column counter
+    if (colCount <= gridSize){
+      board.cells[a].col = colNumber;
+      colCount++;
+    }
+
+    //If number of columns is greater than gridSize then reset columns to 1
+    if (colCount >= gridSize){
+      colNumber++;
+      colCount = 1;
+    }
+
+} */
+
+
+
+
+
 function startGame () {
+
+
   for (var i = 0; i < board.cells.length; i++){
         board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
@@ -114,14 +146,3 @@ function countSurroundingMines (cell) {
   }
   return count;
 };
-
-
-    /* console.log(surrounding);
-    surrounding.forEach(function(cells){
-      if(board.cells.isMine){
-        count++;
-      }
-    })
-
-  return count;
-}; */
